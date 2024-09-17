b1c283acc663384e9885c9effe36dc0d118b71a4
Ayak32 <ayakasim8@gmail.com>
2024-09-17 23:56:54 +0000

    Implementing Tests

--- lab0/merge_test.go
+++ lab0/merge_test.go
@@ -32,12 +32,56 @@ func runMergeTest(t *testing.T, merge mergeFunc) {
 		require.Empty(t, chanToSlice(out))
 	})
 
-	// Please write your own tests
+
 }
 
 func TestMergeChannels(t *testing.T) {
 	runMergeTest(t, func(a, b, out chan string) {
 		lab0.MergeChannels(a, b, out)
+	})
+
+	// NEW TEST
+	t.Run("alternating sends", func(t *testing.T) {
+		a := make(chan string)
+		b := make(chan string)
+		out := make(chan string, 4)
+
+		go func() {
+			a <- "a1"
+			b <- "b1"
+			a <- "a2"
+			b <- "b2"
+			close(a)
+			close(b)
+		}()
+		
+
+		lab0.MergeChannels(a, b, out)
+		// If your lab0 hangs here, make sure you are closing your channels!
+		require.Equal(t, []string{"a1", "b1", "a2", "b2"}, chanToSlice(out))
+	})
+
+
+	// NEW TEST
+	t.Run("one closed channel", func(t *testing.T) {
+		a := make(chan string)
+		b := make(chan string)
+		out := make(chan string, 4)
+	
+
+		go func() {
+			a <- "a1"
+			close(a)
+			b <- "b1"
+			b <- "b2"
+			b <- "b3"
+			close(b)
+		}()
+		
+
+		lab0.MergeChannels(a, b, out)
+		// If your lab0 hangs here, make sure you are closing your channels!
+		require.Equal(t, []string{"a1", "b1", "b2", "b3"}, chanToSlice(out))
 	})
 }
 
@@ -85,6 +129,36 @@ func TestMergeOrCancel(t *testing.T) {
 		require.Error(t, err)
 		require.Equal(t, []string{"a", "b"}, chanToSlice(out))
 	})
+
+	// NEW TEST
+	//checks that canceling the context after the merging does not affect the final output
+	t.Run("cancel after close", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		a := make(chan string, 2)
+		b := make(chan string, 2)
+		out := make(chan string, 10)
+
+		a <- "a1"
+		a <- "a2"
+		b <- "b1"
+		b <- "b2"
+		close(a)
+		close(b)
+
+		eg, _ := errgroup.WithContext(context.Background())
+		eg.Go(func() error {
+			return lab0.MergeChannelsOrCancel(ctx, a, b, out)
+		})
+
+		err := eg.Wait()
+		
+		cancel()
+
+
+		require.NoError(t, err)
+		require.ElementsMatch(t, []string{"a1", "a2", "b1", "b2"}, chanToSlice(out))
+	})
 }
 
 type channelFetcher struct {
@@ -106,6 +180,27 @@ func TestMergeFetches(t *testing.T) {
 	})
 }
 
+type emptyFetcher struct{}
+
+func (f *emptyFetcher) Fetch() (string, bool) {
+    return "", false // Always returns false, indicating no data
+}
+
 func TestMergeFetchesAdditional(t *testing.T) {
 	// TODO: add your extra tests here
+	runMergeTest(t, func(a, b, out chan string) {
+		lab0.MergeFetches(newChannelFetcher(a), newChannelFetcher(b), out)
+	})
+
+	t.Run("one closed channel", func(t *testing.T) {
+		a := &emptyFetcher{}
+		b := &emptyFetcher{}
+		out := make(chan string)
+
+
+		lab0.MergeFetches(a, b, out)
+		// If your lab0 hangs here, make sure you are closing your channels!
+		require.Equal(t, []string{"a1", "b1", "b2", "b3"}, chanToSlice(out))
+	})
+
 }
